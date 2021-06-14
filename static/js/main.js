ff6cfd8e78a632eda03c75d1da808c029e103184
nyamzy <john.nyamweya@student.moringaschool.com>
2021-06-14 09:08:32 +0300

    created functions in the main.js file

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,27 @@
 function getDayIndex(day, month, year) {
   var dd = day;
   var mm = month;
-  var 
+  var temp = year.split("");
+  var temp2 = temp.slice()
+  var yy = parseInt("".join(((temp.split("")).slice(0,2))))
+  var cc = parseInt("".join(((temp2.split("")).slice(2,2))))
+  var dayIndex = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
+}
+
+function getDayAndName(){
+  var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+  let index = getDayIndex(dd,mm,yy);
+  let day = dayIndex;
+  let gender = "";
+  let name = "";
+
+  if (gender === "male"){
+    return maleNames;
+  }else {
+    return femaleNames;
+  }
+return 
 }
